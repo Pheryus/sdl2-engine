883640e3a8511e427b0d051f836cb5e8143e23f8
Pheryus <pheryus@hotmail.com>
2016-04-19 02:10:30 -0300

    correcao main

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,29 @@
-//==============================================================================
-/*
-	Run the app!
 
-	3/11/2014
-    SDLTutorials.com
-    Tim Jones
-*/
-//==============================================================================
+#undef main
 #include "System.h"
+#include <sqlite3.h>
 #include <vector>
 #include "GameObject.h"
-int main(int argc, char* argv[]) {
-	if (App::GetInstance()->Init()){
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "CardGame/Card.h"
+#include "CardGame/Player.h"
+#include "CardGame/Create_Deck.h"
+#include "DataBase/DataBase.h"
+
+using namespace std;
+//Screen dimension constant
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+
+int main(int argc, char* argv[])
+{
+   std::vector<Card*> aux = DataBase::Init("cartas.db");
+   if (App::GetInstance()->Init()){
 		GameObject* vinnie = new GameObject("Test");
 		std::vector<GameObject*> gebinha;
 		gebinha.push_back(new GameObject("Test"));
